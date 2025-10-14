679659470517adf39fd89255307bcee48e30ea5a
SteveRonald <okothroni863@gmail.com>
2025-10-14 19:25:29 +0300

    commit

--- src/config/api.ts
+++ src/config/api.ts
@@ -29,7 +29,7 @@ const getApiBaseUrl = () => {
   }
   
   // Production mode - you can set your production URL here
-   return 'https://poultryhubkenya.great-site.net';
+   return 'https://poultryhubkenya.great-site.net/backend';
 };
 
 export const API_BASE_URL = getApiBaseUrl();
